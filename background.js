3807ab5e4d014fee1df760fe7594246e90171b66
cam song <neosoyn@gmail.com>
2013-12-17 23:59:35 +0800

    bump to version 0.3.1 refine code structure

--- background.js
+++ background.js
@@ -1,76 +1,107 @@
-var GitHubNotification = (function(){
+;(function(){
+'use strict';
 
-  var showBadgeOnZero = true;
-  var interval = 30000;
+var GitHubNotification;
+var notificationUrl = 'https://github.com/notifications';
+var blue = [1, 128, 255, 255];
+var gray = [190, 190, 190, 230];
 
-  function extractUnreadNotifications(response) {
-    var unreadMessages = parseInt(response.match(/<span class="count">([^<]+)</)[1]);
+function _isNotificationUrl(url) {
+  return url.indexOf(notificationUrl) === 0;
+}
 
-    return unreadMessages;
-  }
+function _goToNotificationTab() {
+  console.log('Going to notification tab...');
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    for (var i = 0, tab; tab = tabs[i]; i++) {
+      if (tab.url && _isNotificationUrl(tab.url)) {
+        console.log('Found notification tab: ' + tab.url + '. ' +
+                    'Focusing and refreshing count...');
+        chrome.tabs.update(tab.id, {selected: true});
+        GitHubNotification.checkNotifications();
+        return;
+      }
+    }
+    console.log('Could not find notification tab. Creating one...');
+    chrome.tabs.create({url: notificationUrl});
+  });
+}
 
-  return {
-    BLUE: [1, 128, 255, 255],
-    GRAY: [190, 190, 190, 230],
-    NOTIFICATION_URL: 'https://github.com/notifications',
+function _extractUnreadNotifications(response) {
+  return parseInt(response.match(/<span class="count">([^<]+)</)[1]);
+}
 
-    init: function(){
-      GitHubNotification.checkNotifications();
-      GitHubNotification.checkNotificationsAfterTimeout();
+function _loginWarning() {
+  chrome.browserAction.setBadgeBackgroundColor({color: gray});
+  chrome.browserAction.setBadgeText({text: '?'});
+  chrome.browserAction.setTitle({title: chrome.i18n.getMessage("github_login_needed")});
+}
 
-      chrome.browserAction.onClicked.addListener(function() {
-        window.open(GitHubNotification.NOTIFICATION_URL);
-      });
-    },
+function _getBadgeText(num) {
+  return num != 0 ? num+'' : ''
+}
 
-    checkNotificationsAfterTimeout: function(){
-      GitHubNotification.INTERVAL_BETWEEN_CHECK_NOTIFICATIONS = interval;
+function _getTitle(num) {
+  return num + " unread " +
+         (num == 1 ? 'notification' : 'notifications');
+}
 
-      window.setTimeout(
-        function(){
-          GitHubNotification.checkNotifications();
-          GitHubNotification.checkNotificationsAfterTimeout();
-        },
-        GitHubNotification.INTERVAL_BETWEEN_CHECK_NOTIFICATIONS
-      );
-    },
+function _displayUnreadCount(response) {
+  var unreadCount = _extractUnreadNotifications(response);
+  console.log('Get ' + unreadCount + ' unread notifications');
 
-    checkNotifications: function() {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", GitHubNotification.NOTIFICATION_URL, true);
+  chrome.browserAction.setBadgeBackgroundColor({color: blue});
+  chrome.browserAction.setBadgeText({text: _getBadgeText(unreadCount)});
 
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-          var response = xhr.response;
+  chrome.browserAction.setTitle({title: _getTitle(unreadCount)});
+}
 
+GitHubNotification = {
+  config: {
+    interval: 30000,
+  },
+
+  init: function() {
+    this.checkNotifications();
+    this.checkNotificationsLoop();
+
+    chrome.browserAction.onClicked.addListener(this.goToNotificationTab.bind(this));
+  },
+
+  goToNotificationTab: _goToNotificationTab,
+
+  checkNotificationsLoop: function(){
+    window.setTimeout(
+      function(){
+        GitHubNotification.checkNotifications();
+        GitHubNotification.checkNotificationsLoop();
+      },
+      GitHubNotification.config.interval
+    );
+  },
+
+  checkNotifications: function() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        var response = xhr.response;
+
+        if (response.match(/<body class="logged_out/)) {
           // not logged in
-          if (response.match(/<body class="logged_out/)) {
-            chrome.browserAction.setBadgeBackgroundColor({color: GitHubNotification.GRAY});
-            chrome.browserAction.setBadgeText({text: '?'});
-            chrome.browserAction.setTitle({title: chrome.i18n.getMessage("github_login_needed")});
-            return;
-          }
-
+          console.log('not logged in currently');
+          _loginWarning();
+        } else {
           // logged in
-          var unreadNotifications = extractUnreadNotifications(response) + "";
-
-          chrome.browserAction.setBadgeBackgroundColor({color: GitHubNotification.BLUE});
-
-          chrome.browserAction.setBadgeText({
-            text: unreadNotifications != "0" ? unreadNotifications : ''
-          });
-
-          var notificationsInflection = (unreadNotifications == 1 ?
-              'notification' : 'notifications');
-
-          var title = unreadNotifications + " unread " + notificationsInflection;
-          chrome.browserAction.setTitle({title: title});
+          _displayUnreadCount(response);
         }
       }
-      xhr.send(null);
-    }
-  };
-
-})();
+    };
+    xhr.open("GET", notificationUrl, true);
+    console.log('making request to get notifications...');
+    xhr.send(null);
+  }
+};
 
 GitHubNotification.init();
+
+})();
--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-var FileDownloader = (function(){
-  return {
-    init: function() {
-      document.addEventListener('click', FileDownloader.eventHandler);
-    },
-    eventHandler: function(event) {
-      if (FileDownloader.fromListPage(event.target)) {
-        var linkNode = event.target.parentNode.nextElementSibling.querySelector('a');
-        var href = linkNode.href.replace('github.com', 'raw.github.com').replace('\/blob\/', '\/');
-        FileDownloader.downloadIt(href, linkNode.textContent);
-      } else if (FileDownloader.fromFilePage(event.target) && document.querySelector('#raw-url')) {
-        var href = document.querySelector('#raw-url').href;
-        var fileName = document.querySelector('.breadcrumb .final-path').textContent;
-        FileDownloader.downloadIt(href, fileName);
-      }
-    },
-    downloadIt: function(href, fileName) {
-      var downloadNode = document.createElement('a');
-      downloadNode.setAttribute('href', href);
-      downloadNode.setAttribute('download', fileName);
-      downloadNode.click();
-      downloadNode = null;
-    },
-    fromListPage: function(node) {
-      return node.classList.contains('octicon-file-text') &&
-        document.querySelector('.files') &&
-        document.querySelector('.files').contains(node) &&
-        document.querySelector('.file') === null;
-    },
-    fromFilePage: function(node) {
-      return node.classList.contains('octicon-file-text') &&
-        document.querySelector('.file') &&
-        document.querySelector('.file').contains(node);
+var FileDownloader = {
+  init: function() {
+    document.addEventListener('click', this.eventHandler.bind(this));
+  },
+
+  eventHandler: function(event) {
+    if (this.isFromListPage(event.target)) {
+      var linkNode = event.target.parentNode.nextElementSibling.querySelector('a');
+      var href = linkNode.href.replace('github.com', 'raw.github.com').replace('\/blob\/', '\/');
+      this.downloadIt(href, linkNode.textContent);
+    } else if (this.isFromFilePage(event.target) && document.querySelector('#raw-url')) {
+      var href = document.querySelector('#raw-url').href;
+      var fileName = document.querySelector('.breadcrumb .final-path').textContent;
+      this.downloadIt(href, fileName);
     }
-  };
-})();
+  },
+
+  downloadIt: function(href, fileName) {
+    var downloadNode = document.createElement('a');
+    downloadNode.setAttribute('href', href);
+    downloadNode.setAttribute('download', fileName);
+    downloadNode.click();
+    downloadNode = null;
+  },
+
+  isFromListPage: function(node) {
+    return node.classList.contains('octicon-file-text') &&
+      document.querySelector('.files') &&
+      document.querySelector('.files').contains(node) &&
+      document.querySelector('.file') === null;
+  },
+
+  isFromFilePage: function(node) {
+    return node.classList.contains('octicon-file-text') &&
+      document.querySelector('.file') &&
+      document.querySelector('.file').contains(node);
+  }
+}
 
 FileDownloader.init();
 
