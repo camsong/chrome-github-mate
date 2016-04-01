d2d002f7c09944c71adceb59fbe449d8e62c279f
Pavel Zbytovský <zbytovsky@gmail.com>
2016-04-01 21:56:18 +0200

    favorites panel - edit instead of remove

--- page.js
+++ page.js
@@ -13,17 +13,17 @@ if (panel.length === 0) {
     panel = $('<div class="gm_panel"></div>').html(
         "<a href='#' class='add btn'>add this page</a>" +
         "<b>Favorites</b>" +
-        "<ul></ul>" +
-        "<a href='https://rawgit.com/zbycz/github-events-viewer/master/github-event-viewer.html'>My events viewer</a>" +
+        "<ul class='favorites'></ul>" +
         "<style>" +
         ".gm_panel { position: fixed; top:0; right: 0; z-index: 10;" +
-        "   background:rgba(255,255,255,0.9); border-left: 3px #d26911 solid;border-radius: 3px; width: 200px; padding: 10px 5px 15px 15px}" +
+        "   background:rgba(255,255,255,0.9); border-left: 3px #d26911 solid;border-radius: 3px; width: 200px; padding: 10px 5px 15px 15px;" +
+        "   max-height:100%;overflow-y:auto}" +
         "@media (max-width: 1400px){ .gm_panel { right:-185px; } .gm_panel:hover { right:0; } }" +
         ".gm_panel a.add.btn { font-size: 10px; float: right; padding: 2px 7px; }" +
         ".gm_panel b { line-height: 30px }" +
-        ".gm_panel ul { margin-bottom: 2em; }" +
+        ".gm_panel ul.favorites { margin-bottom: 2em; }" +
         ".gm_panel li { margin: .4em 0 0 1em; }" +
-        ".gm_panel li span.rem { display: none; position: absolute; right:5px; cursor: pointer; }" +
+        ".gm_panel li span.edit { display: none; position: absolute; right:5px; cursor: pointer; }" +
         ".gm_panel li:hover span { display: inline; }" +
         "</style>"
     );
@@ -50,29 +50,38 @@ if (panel.length === 0) {
         return false;
     });
 
-    panel.on('click', 'span.rem', function () {
+    panel.on('click', 'span.edit', function () {
         var items = gm_get_favorites();
-        items.splice($(this).attr('data-i'), 1);
+        var i = $(this).attr('data-i');
+
+        var pr = window.prompt('Set new title OR set blank to delete', items[i].text);
+        if (pr === null) return;
+        else if (pr === '') items.splice(i, 1);
+        else items[i].text = pr;
+
         gm_set_favorites(items);
         gm_refresh_favorites();
     });
 }
 
-
+var lastString = '';
 function gm_refresh_favorites() {
     var ul = '';
     var items = gm_get_favorites();
     for (var i in items) {
         var item = items[i];
         //var text = decodeURIComponent(item.loc.replace(/https?:\/\/[^\/]+\//, '')).replace('+', ' ');
-        ul += '<li><span data-i="' + i + '" class="rem">&times;</span>  <a href="' + item.loc + '" title="' + item.title + '">' + item.text + '</a></li>'
+        ul += '<li><span data-i="' + i + '" class="edit">&#9998;</span>  <a href="' + item.loc + '" title="' + item.title + '">' + item.text + '</a></li>'
     }
-    panel.find('ul').html(ul);
+    if (ul != lastString) {
+        lastString = ul;
+        panel.find('ul.favorites').html(ul);
+    }
 }
 
 
 function gm_get_favorites() {
-    return JSON.parse(localStorage.getItem('gm_panel')) || [];
+    return JSON.parse(localStorage.getItem('gm_panel')) || [{loc: 'https://rawgit.com/zbycz/github-events-viewer/master/github-event-viewer.html', text: 'My GH events', title:'default entry'}];
 }
 function gm_set_favorites(items) {
     localStorage.setItem('gm_panel', JSON.stringify(items));
