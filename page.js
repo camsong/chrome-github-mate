934cddaffb773ff20707bbc470508fec260d93db
Pavel Zbytovský <zbytovsky@gmail.com>
2016-04-01 21:58:46 +0200

    Show markdown outline in side panel

--- page.js
+++ page.js
@@ -3,9 +3,13 @@ $(document).on('pjax:end', function () {
     var e = document.createEvent('Events');
     e.initEvent('_pjax:end', !'bubbles', !'cancelable');
 
+    updateOutlinesInPanel()
+
     return document.dispatchEvent(e);
 });
 
+
+// Sidebar panel originaly by github.com/zbycz
 
 var panel = $('div.gm_panel');
 
@@ -25,7 +29,9 @@ if (panel.length === 0) {
         ".gm_panel li { margin: .4em 0 0 1em; }" +
         ".gm_panel li span.edit { display: none; position: absolute; right:5px; cursor: pointer; }" +
         ".gm_panel li:hover span { display: inline; }" +
-        "</style>"
+        "</style>" +
+        "<div class='gm_outline'></div>"
+
     );
     $('body').append(panel);
 
@@ -86,3 +92,78 @@ function gm_get_favorites() {
 function gm_set_favorites(items) {
     localStorage.setItem('gm_panel', JSON.stringify(items));
 }
+
+
+
+
+// Copyright (c) 2015 Dan Kaplun
+// The MIT License (MIT)
+// from https://github.com/dbkaplun/github-markdown-outline-extension/blob/master/index.js
+// very hackily edited by zbycz to add it in panel
+
+getHeaderLevel.REGEXP = /h(\d)/i
+function getHeaderLevel ($h) {
+    var level = Number(((($h || {}).tagName || '').match(getHeaderLevel.REGEXP) || [])[1])
+    return isNaN(level) ? undefined : level
+}
+
+var headerSels = []
+for (var l = 1; l <= 6; l++) headerSels.push('h'+l)
+var headerSel = headerSels.join(', ')
+var anchorSel = 'a[id]'
+
+function updateOutlinesInPanel() {
+    var $container = document.createElement('div')
+    $container.classList.add('gm_outline')
+
+    Array.from(document.querySelectorAll('.markdown-body')).forEach($md => {
+        var $headers = Array.from($md.querySelectorAll(headerSel))
+
+
+        var $b = document.createElement('b')
+        $b.innerText = "Outline"
+        $container.appendChild($b)
+
+        var $outline = document.createElement('ul')
+        //$outline.classList.add('__github-markdown-outline')
+        $container.appendChild($outline)
+
+        // generate outline from headers
+        $headers.forEach($h => {
+            var level = getHeaderLevel($h)
+            if (!level) return
+            var $ul = $outline, $li, $child
+            for (var l = 1; l < level; l++) {
+                $li = $ul.lastChild || $ul.appendChild(document.createElement('li'))
+                $child = $li.lastChild || {}
+                $ul = $child.tagName === 'UL'
+                    ? $child
+                    : $li.appendChild(document.createElement('ul'))
+            }
+            var $topic = $ul
+                .appendChild(document.createElement('li'))
+                .appendChild(document.createElement('a'))
+            $topic.innerText = $h.innerText
+            $topic.href = `#${$h.querySelector(anchorSel).id.replace(/^user-content-/, '')}`
+        })
+
+        // find all sublists with one item and replace with contents
+        Array.from($container.querySelectorAll('ul')).forEach($ul => {
+            var $parent = $ul.parentNode
+            var $li = $ul.firstChild
+            var $child = $li.firstChild
+            if ($li !== $ul.lastChild || $child.tagName !== 'UL') return
+            while ($child) {
+                $parent.insertBefore($child, $ul.nextSibling) // inserts to end of list if $ul.nextSibling is null
+                $child = $child.nextSibling
+            }
+            $parent.removeChild($ul)
+        })
+
+    })
+
+    //$md.insertBefore($container, $md.firstChild)
+    panel.find('.gm_outline').replaceWith($container);
+}
+
+updateOutlinesInPanel();
