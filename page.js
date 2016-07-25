173f9e52b5b2394bbdf92a1c77950e529a70a250
Cam Song <neosoyn@gmail.com>
2016-07-25 10:57:59 +0800

    fix: Uncaught TypeError: $ is not a function

--- page.js
+++ page.js
@@ -1,8 +1,7 @@
-$(document).on('pjax:end', function () {
+document.addEventListener('pjax:end', function () {
     // Duplicate the event with a slightly different name
     var e = document.createEvent('Events');
     e.initEvent('_pjax:end', !'bubbles', !'cancelable');
 
     return document.dispatchEvent(e);
 });
-
